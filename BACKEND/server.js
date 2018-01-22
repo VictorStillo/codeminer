a4306e64a032cb7a9c88b3fd1955f0e8c449e9b5
Victor <victor.stillo@gmail.com>
2018-01-22 19:47:49 -0200

    Correct trade

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -14,6 +14,9 @@ mongoose.connect('mongodb://localhost/codeminer');
 var db = mongoose.connection;
 
 
+var trader1_available = false;
+var trader2_available = false;
+
 //Routes for our API
 var router = express.Router();
 
@@ -65,6 +68,7 @@ router.route('/survivors')
 //Routes that ends in /survivors/:survivor_id
 router.route('/survivors/:survivor_id')
 
+    //Get a survivor's data
     .get(function(req, res){
         Survivor.findById(req.params.survivor_id, function(err, survivor){
             if(err)
@@ -73,6 +77,7 @@ router.route('/survivors/:survivor_id')
         });
     })
 
+    //UPDATE a survivor's location
     .put(function(req, res){
         Survivor.findById(req.params.survivor_id, function(err, survivor) {
 
@@ -93,6 +98,7 @@ router.route('/survivors/:survivor_id')
         })
     })
 
+    //DELETE a survivor's data
     .delete(function(req, res) {
         Survivor.remove({
             _id: req.params.survivor_id
@@ -108,6 +114,7 @@ router.route('/survivors/:survivor_id')
 //ROUTE FOR survivors/:survivor_id/infection
 router.route('/survivors/:survivor_id/infection')
     
+    //Verify infection data of a survivor
     .get(function(req, res){
         Survivor.findById(req.params.survivor_id, function(err, survivor){
             if(err)
@@ -116,6 +123,7 @@ router.route('/survivors/:survivor_id/infection')
         })
     })
 
+    //Report and update a survivor infected an
     .put(function(req, res){
         Survivor.findById(req.body.reportedId, function(err, survivor){
             if(err)
@@ -144,8 +152,10 @@ router.route('/survivors/:survivor_id/infection')
         })
     })
 
+//ROUTE FOR survivors/:survivor_id/inventory
 router.route('/survivors/:survivor_id/inventory')
 
+    //Return an survivor's inventory
     .get(function(req, res){
         Survivor.findById(req.params.survivor_id, function(err, survivor) {
             if(err)
@@ -154,50 +164,139 @@ router.route('/survivors/:survivor_id/inventory')
         })
     })
 
+    //Trade and update inventory items for a survivor
     .put(function(req, res) {
-        
-        Survivor.findById(req.params.survivor_id, function(err, survivor){
 
-            var pickItems = req.body.pickItems;
-            var payItems = req.body.payItems;
+        Survivor.findById(req.body.survivorId1, function(err, survivor1){
 
-            pickItems.value = pickItems.water * 4,
-                                pickItems.food * 3,
-                                pickItems.medication * 2,
-                                pickItems.ammunition * 1;
+            survivor1.inventory.water = Number(survivor1.inventory.water);
+            survivor1.inventory.food = Number(survivor1.inventory.food);
+            survivor1.inventory.medication = Number(survivor1.inventory.medication);
+            survivor1.inventory.ammunition = Number(survivor1.inventory.ammunition);
+
+            var pickItems = 0;
+            var payItems = 0;
+
+            var water1 = Number(req.body.water1);
+            var food1 = Number(req.body.food1);
+            var medication1 = Number(req.body.medication1);
+            var ammunition1 = Number(req.body.ammunition1);
+
+            var water2 = Number(req.body.water2);
+            var food2 = Number(req.body.food2);
+            var medication2 = Number(req.body.medication2);
+            var ammunition2 = Number(req.body.ammunition2);
+
+            pickItems = water2 * 4
+                            + food2 * 3
+                            + medication2 * 2
+                            + ammunition2 * 1;
             
-            payItems.value = payItems.water * 4,
-                                payItems.food * 3,
-                                payItems.medication * 2,
-                                payItems.ammunition * 1;
+            payItems = water1 * 4
+                            + food1 * 3
+                            + medication1 * 2
+                            + ammunition1 * 1;
 
-            if(pickItems.value == payItems.value){
-                if(survivor.inventory.water >= pickItems.water &&
-                    survivor.inventory.food >= pickItems.food &&
-                    survivor.inventory.medication >= pickItems.medication &&
-                    survivor.inventory.ammunition >= pickItems.ammunition &&
-                    !survivor.infection.isInfected){
-                        survivor.inventory.water += pickItems.water;
-                        survivor.inventory.food += pickItems.food;
-                        survivor.inventory.medication += pickItems.medication;
-                        survivor.inventory.ammunition += pickItems.ammunition;
+            
+            if(pickItems == payItems){
+                if(survivor1.inventory.water >= water1 &&
+                    survivor1.inventory.food >= food1 &&
+                    survivor1.inventory.medication >= medication1 &&
+                    survivor1.inventory.ammunition >= ammunition1 &&
+                    !survivor1.infection.isInfected){
+                        survivor1.inventory.water += water2;
+                        survivor1.inventory.food += food2;
+                        survivor1.inventory.medication += medication2;
+                        survivor1.inventory.ammunition += ammunition2;
 
-                        survivor.inventory.water -= payItems.water;
-                        survivor.inventory.food -= payItems.food;
-                        survivor.inventory.medication -= payItems.medication;
-                        survivor.inventory.ammunition -= payItems.ammunition;
+                        survivor1.inventory.water -= water1;
+                        survivor1.inventory.food -= food1;
+                        survivor1.inventory.medication -= medication1;
+                        survivor1.inventory.ammunition -= ammunition1;
 
-                        res.json({ message: "Troca realizada com sucesso!"})
+                        trader1_available = true;
+                }
+                
+            }
 
+            Survivor.findById(req.body.survivorId2, function(err, survivor2){
+
+                survivor2.inventory.water = Number(survivor1.inventory.water);
+                survivor2.inventory.food = Number(survivor1.inventory.food);
+                survivor2.inventory.medication = Number(survivor1.inventory.medication);
+                survivor2.inventory.ammunition = Number(survivor1.inventory.ammunition);
+
+                var pickItems = 0;
+                var payItems = 0;
+    
+                var water1 = Number(req.body.water1);
+                var food1 = Number(req.body.food1);
+                var medication1 = Number(req.body.medication1);
+                var ammunition1 = Number(req.body.ammunition1);
+    
+                var water2 = Number(req.body.water2);
+                var food2 = Number(req.body.food2);
+                var medication2 = Number(req.body.medication2);
+                var ammunition2 = Number(req.body.ammunition2);
+    
+                pickItems = water1 * 4
+                                + food1 * 3
+                                + medication1 * 2
+                                + ammunition1 * 1;
+                
+                payItems = water2 * 4
+                                + food2 * 3
+                                + medication2 * 2
+                                + ammunition2 * 1;
+    
+                
+                if(pickItems == payItems){
+                    if(survivor2.inventory.water >= water2 &&
+                        survivor2.inventory.food >= food2 &&
+                        survivor2.inventory.medication >= medication2 &&
+                        survivor2.inventory.ammunition >= ammunition2 &&
+                        !survivor2.infection.isInfected){
+                            survivor2.inventory.water += water1;
+                            survivor2.inventory.food += food1;
+                            survivor2.inventory.medication += medication1;
+                            survivor2.inventory.ammunition += ammunition1;
+    
+                            survivor2.inventory.water -= water2;
+                            survivor2.inventory.food -= food2;
+                            survivor2.inventory.medication -= medication2;
+                            survivor2.inventory.ammunition -= ammunition2;
+    
+                            trader2_available = true;
+                    }
+                }
+                if(trader1_available && trader2_available){
+                    survivor1.save(function(err){
+                        if(err)
+                            res.send(err)
+                    })
+
+                    survivor2.save(function(err){
+                        if(err)
+                            res.send(err)
+                    })
+
+                    res.json("Troca Realizada com sucesso!");
                 }
                 else{
-                    res.json({ message: "Não foi possível completar a troca, items insuficientes ou usuário infectado."})
+                    res.json("Não foi possível realizar a troca!");
                 }
-            }
+                
+            })
+            
         })
+        
+        
     })
 
+//ROUTE FOR /reports
 router.route('/reports')
+
+    //Generate reports
     .get(function(req, res){
         Survivor.find(function(err, survivors) {
             if (err)
